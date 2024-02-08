3bb9f0f625e4d00b851529f10a4bdec7d4dc7590
Stefania Veronica Bruera <59149@sistemas.frc.utn.edu.ar>
2024-02-07 22:15:00 -0300

    Test link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-// https://api.openweathermap.org/data/2.5/forecast?q=cordoba&appid=cd01bf34d4ef941e71683efacb86b4c7&cnt=56
+// https://api.openweathermap.org/data/2.5/forecast?q=cordoba&appid=cd01bf34d4ef941e71683efacb86b4c7&cnt=56&lang=es
 
 export default function Home () {
   return (
